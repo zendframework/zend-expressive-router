60f2ca72637c279e7076e216c3c72528d2928102
Michael Moussa <michael.moussa@gmail.com>
2016-10-03 21:47:21 -0400

    RouterInterface tweaks
    Removed the RuntimeConfigurableRouterInterface and put addRoute back into
    the RouterInterface. We'll allow the `$options` to be passed on to the
    actual routers via the new third `$options` parameter.
    
    This approach does break BC as well, but it will be released with a new
    major version to avoid surprising library users.

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -17,6 +17,26 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 interface RouterInterface
 {
     /**
+     * Add a route.
+     *
+     * This method adds a route against which the underlying implementation may
+     * match. Implementations MUST aggregate route instances, but MUST NOT use
+     * the details to inject the underlying router until `match()` and/or
+     * `generateUri()` is called.  This is required to allow consumers to
+     * modify route instances before matching (e.g., to provide route options,
+     * inject a name, etc.).
+     *
+     * The method MUST raise Exception\RuntimeException if called after either `match()`
+     * or `generateUri()` have already been called, to ensure integrity of the
+     * router between invocations of either of those methods.
+     *
+     * @param Route $route
+     * @throws Exception\RuntimeException when called after match() or
+     *     generateUri() have been called.
+     */
+    public function addRoute(Route $route);
+
+    /**
      * Match a request against the known routes.
      *
      * Implementations will aggregate required information from the provided
